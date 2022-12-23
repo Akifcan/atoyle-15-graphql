6e18fa9a07d0eb622b3bd881846831916fbe1a1d
Akifcan Kara <akfkara97@gmail.com>
2022-12-24 01:18:23 +0300

    dist file

--- dist/modules/user/user.resolver.js
+++ dist/modules/user/user.resolver.js
@@ -1,36 +1,64 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userResolvers = void 0;
-const db_postgres_1 = __importDefault(require("../../lib/db/db.postgres"));
-exports.userResolvers = {
-    hello: () => {
-        return 'Hello world!';
-    },
-    demo: () => __awaiter(void 0, void 0, void 0, function* () {
-        try {
-            const users = yield db_postgres_1.default.client.query('SELECT * FROM employee');
-            console.log(users.rows);
-            return 17;
-        }
-        catch (e) {
-            console.log(e);
-            throw new Error();
-        }
-    }),
-    signIn: (props) => {
-        const { user } = props;
-        return user;
+'use strict'
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value)
+          })
     }
-};
+    return new (P || (P = Promise))(function (resolve, reject) {
+      function fulfilled(value) {
+        try {
+          step(generator.next(value))
+        } catch (e) {
+          reject(e)
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value))
+        } catch (e) {
+          reject(e)
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected)
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next())
+    })
+  }
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod }
+  }
+Object.defineProperty(exports, '__esModule', { value: true })
+exports.userResolvers = void 0
+const db_postgres_1 = __importDefault(require('../../lib/db/db.postgres'))
+exports.userResolvers = {
+  hello: () => {
+    return 'Hello world!'
+  },
+  demo: () =>
+    __awaiter(void 0, void 0, void 0, function* () {
+      try {
+        const users = yield db_postgres_1.default.client.query(
+          'SELECT * FROM employee'
+        )
+        console.log(users.rows)
+        return 17
+      } catch (e) {
+        console.log(e)
+        throw new Error()
+      }
+    }),
+  signIn: (props) => {
+    const { user } = props
+    return user
+  }
+}
