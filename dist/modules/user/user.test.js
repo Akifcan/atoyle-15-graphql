450a59d18005afc28f82fff106cd960b41c83878
Akifcan Kara <akfkara97@gmail.com>
2022-12-24 10:15:11 +0300

    add user unit tests

--- dist/modules/user/user.test.js
+++ dist/modules/user/user.test.js
@@ -14,6 +14,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const db_postgres_1 = __importDefault(require("../../lib/db/db.postgres"));
 const startup_1 = __importDefault(require("../../lib/startup"));
+const test_1 = require("../../test");
+const user_resolver_1 = require("./user.resolver");
 describe('Test the root path', () => {
     beforeAll(() => __awaiter(void 0, void 0, void 0, function* () {
         yield (0, startup_1.default)();
@@ -21,7 +23,79 @@ describe('Test the root path', () => {
     afterAll(() => __awaiter(void 0, void 0, void 0, function* () {
         yield db_postgres_1.default.client.end();
     }));
-    test('It should return 17', () => __awaiter(void 0, void 0, void 0, function* () {
-        expect(17).toBe(17);
+    test('It should sign in when credentials are correct', () => __awaiter(void 0, void 0, void 0, function* () {
+        const user = yield user_resolver_1.userResolvers.signIn({
+            email: 'akif@mail.com',
+            password: '124'
+        });
+        expect(user).toHaveProperty('name');
+        expect(user).toHaveProperty('email');
+        expect(user).toHaveProperty('id');
+        expect(user).toHaveProperty('token');
+    }));
+    test('It should throw error if credentials are wrong', () => __awaiter(void 0, void 0, void 0, function* () {
+        try {
+            yield user_resolver_1.userResolvers.signIn({
+                email: 'akiasdfasdf@mail.com',
+                password: '124'
+            });
+            expect(true).toBe(false);
+        }
+        catch (e) {
+            expect(e).toBeInstanceOf(Error);
+            expect(e).toHaveProperty('message', 'This user not found');
+        }
+    }));
+    test('It should throw error if inputs are not valid', () => __awaiter(void 0, void 0, void 0, function* () {
+        try {
+            yield user_resolver_1.userResolvers.signIn({
+                email: 'akia',
+                password: '1asdf24'
+            });
+            expect(true).toBe(false);
+        }
+        catch (e) {
+            expect(e).toBeInstanceOf(Error);
+            expect(e).toHaveProperty('message');
+        }
+    }));
+    test('It should return profile props', () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield user_resolver_1.userResolvers.profile({ slug: 'akif-kara' }, { headers: { authorization: test_1.MOCK_JWT } });
+        expect(response).toHaveProperty('id');
+        expect(response).toHaveProperty('name');
+        expect(response).toHaveProperty('department');
+        expect(response).toHaveProperty('description');
+        expect(response).toHaveProperty('email');
+        expect(response).toHaveProperty('slug');
+    }));
+    test('It should throw error if profile does not found', () => __awaiter(void 0, void 0, void 0, function* () {
+        try {
+            yield user_resolver_1.userResolvers.profile({ slug: 'asdfasdfasdf' }, { headers: { authorization: test_1.MOCK_JWT } });
+            expect(true).toBe(false);
+        }
+        catch (e) {
+            expect(e).toBeInstanceOf(Error);
+            expect(e).toHaveProperty('message', 'This user not found');
+        }
+    }));
+    test('It should throw error if no jwt provided', () => __awaiter(void 0, void 0, void 0, function* () {
+        try {
+            yield user_resolver_1.userResolvers.profile({ slug: 'asdfasdfasdf' }, { headers: { authorization: '' } });
+            expect(true).toBe(false);
+        }
+        catch (e) {
+            expect(e).toBeInstanceOf(Error);
+            expect(e).toHaveProperty('message', 'jwt must be provided');
+        }
+    }));
+    test('It should throw error if jwt regex is wrong', () => __awaiter(void 0, void 0, void 0, function* () {
+        try {
+            yield user_resolver_1.userResolvers.profile({ slug: 'asdfasdfasdf' }, { headers: { authorization: 'fasdfasdf' } });
+            expect(true).toBe(false);
+        }
+        catch (e) {
+            expect(e).toBeInstanceOf(Error);
+            expect(e).toHaveProperty('message', 'jwt malformed');
+        }
     }));
 });
--- modules/user/user.test.ts
+++ modules/user/user.test.ts
@@ -1,5 +1,7 @@
 import Db from '../../lib/db/db.postgres'
 import startup from '../../lib/startup'
+import { MOCK_JWT } from '../../test'
+import { userResolvers } from './user.resolver'
 
 describe('Test the root path', () => {
   beforeAll(async () => {
@@ -10,7 +12,96 @@ describe('Test the root path', () => {
     await Db.client.end()
   })
 
-  test('It should return 17', async () => {
-    expect(17).toBe(17)
+  test('It should sign in when credentials are correct', async () => {
+    const user = await userResolvers.signIn({
+      email: 'akif@mail.com',
+      password: '124'
+    })
+
+    expect(user).toHaveProperty('name')
+    expect(user).toHaveProperty('email')
+    expect(user).toHaveProperty('id')
+    expect(user).toHaveProperty('token')
+  })
+
+  test('It should throw error if credentials are wrong', async () => {
+    try {
+      await userResolvers.signIn({
+        email: 'akiasdfasdf@mail.com',
+        password: '124'
+      })
+      expect(true).toBe(false)
+    } catch (e: any) {
+      expect(e).toBeInstanceOf(Error)
+      expect(e).toHaveProperty('message', 'This user not found')
+    }
+  })
+
+  test('It should throw error if inputs are not valid', async () => {
+    try {
+      await userResolvers.signIn({
+        email: 'akia',
+        password: '1asdf24'
+      })
+      expect(true).toBe(false)
+    } catch (e: any) {
+      expect(e).toBeInstanceOf(Error)
+      expect(e).toHaveProperty('message')
+    }
+  })
+
+  test('It should return profile props', async () => {
+    const response = await userResolvers.profile(
+      { slug: 'akif-kara' },
+      { headers: { authorization: MOCK_JWT } }
+    )
+
+    expect(response).toHaveProperty('id')
+    expect(response).toHaveProperty('name')
+    expect(response).toHaveProperty('department')
+    expect(response).toHaveProperty('description')
+    expect(response).toHaveProperty('email')
+    expect(response).toHaveProperty('slug')
+  })
+
+  test('It should throw error if profile does not found', async () => {
+    try {
+      await userResolvers.profile(
+        { slug: 'asdfasdfasdf' },
+        { headers: { authorization: MOCK_JWT } }
+      )
+      expect(true).toBe(false)
+    } catch (e) {
+      expect(e).toBeInstanceOf(Error)
+      expect(e).toHaveProperty('message', 'This user not found')
+    }
+  })
+
+  test('It should throw error if no jwt provided', async () => {
+    try {
+      await userResolvers.profile(
+        { slug: 'asdfasdfasdf' },
+        { headers: { authorization: '' } }
+      )
+
+      expect(true).toBe(false)
+    } catch (e) {
+      expect(e).toBeInstanceOf(Error)
+      expect(e).toHaveProperty('message', 'jwt must be provided')
+    }
+  })
+
+  test('It should throw error if jwt regex is wrong', async () => {
+    try {
+      await userResolvers.profile(
+        { slug: 'asdfasdfasdf' },
+        { headers: { authorization: 'fasdfasdf' } }
+      )
+
+      expect(true).toBe(false)
+    } catch (e) {
+      expect(e).toBeInstanceOf(Error)
+      expect(e).toHaveProperty('message', 'jwt malformed')
+    }
   })
 })
